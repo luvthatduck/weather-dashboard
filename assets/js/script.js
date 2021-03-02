9f73f6312406c9f4f91b9dcc4e63915488ce300e
Janell Myrick <76677736+luvthatduck@users.noreply.github.com>
2021-03-01 18:54:18 -0700

    changing outline

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,33 @@
+var body = document.body;
+// var header = document.header
+
 var searchBtn = document.getElementById("button-addon2")
 var city = []
+
+// var h1El = document.createElement('h1');
+// h1El.textContent = 'Weather Dashboard Area';
+// h1El.setAttribute('style', 'margin:auto; width:50%; text-align:center;');
+// header.appendChild(h1El);
 
 searchBtn.addEventListener("click", function () {
   var cityInput = document.getElementById("city-input")
   // console.log(cityInput.value)
   city.push(cityInput.value)
-  
+
   localStorage.setItem('cityName', city)
   cityHistory()
-})
+});
 
-var cityHistory = function(){
+var cityHistory = function () {
+  var cityHistory = document.getElementById("cityHistory");
   var listofCities = localStorage.getItem("cityName")
   console.log(listofCities.split(","))
+  cityHistory.innerHTML = listofCities
+  // for (var i = 0, cityHistory; cityHistory = city[i]; i++) {
+  //   listofCities += "<li>" + cityHistory + "</li>";
+  // }
+  
 
-}
+};
 
 cityHistory()
