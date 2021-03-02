1ecdf96bda183ad44374234c315da891a01603ad
Janell Myrick <76677736+luvthatduck@users.noreply.github.com>
2021-03-01 23:24:14 -0700

    adding city search API display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,6 @@
-var body = document.body;
-// var header = document.header
-
 var searchBtn = document.getElementById("button-addon2")
 var city = []
 
-// var h1El = document.createElement('h1');
-// h1El.textContent = 'Weather Dashboard Area';
-// h1El.setAttribute('style', 'margin:auto; width:50%; text-align:center;');
-// header.appendChild(h1El);
 
 searchBtn.addEventListener("click", function () {
   var cityInput = document.getElementById("city-input")
@@ -16,6 +9,9 @@ searchBtn.addEventListener("click", function () {
 
   localStorage.setItem('cityName', city)
   cityHistory()
+  weatherFetch(cityInput.value)
+  tempFetch()
+  humidityFetch()
 });
 
 var cityHistory = function () {
@@ -23,11 +19,72 @@ var cityHistory = function () {
   var listofCities = localStorage.getItem("cityName")
   console.log(listofCities.split(","))
   cityHistory.innerHTML = listofCities
-  // for (var i = 0, cityHistory; cityHistory = city[i]; i++) {
-  //   listofCities += "<li>" + cityHistory + "</li>";
-  // }
-  
+
 
 };
 
+function weatherFetch(city) {
+  // when you click on the button the city will call API and display in cityName ID
+  fetch(
+    'https://api.openweathermap.org/data/2.5/weather?q='
+    + city
+    + '&appid=77923bca40e129e0b197958708cf0174')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      var citySearch = document.querySelector('#cityName');
+      citySearch.innerHTML = '<h2>' + response.name + ' (' + new Date().toLocaleString() + ')<h2><img src="http://openweathermap.org/img/wn/' + response.weather[0].icon + '.png" />'
+
+    });
+};
+
+
+function tempFetch(city) {
+  // when you click on the button the city will call API and display in cityName ID
+  fetch(
+    'https://api.openweathermap.org/data/2.5/weather?q='
+    + city
+    + '&appid=77923bca40e129e0b197958708cf0174')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      var cityTemp = document.querySelector('#cityTemp');
+      cityTemp.innerHTML = '<h3>' + response.temp + '<h3>';
+
+    });
+};
+
+function humidityFetch(city) {
+ 
+  fetch(
+    'https://api.openweathermap.org/data/2.5/weather?q='
+    + city
+    + '&appid=77923bca40e129e0b197958708cf0174')
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (response) {
+      var cityHumidity = document.querySelector('#cityHumidity');
+      cityHumidity.innerHTML = '<h3>' + response.main[0].humidity + '<h3>';
+
+    });
+};
+
+// will display city name, date and weather.icon
+//will display temperature main.temp
+// will display humidity main.humidty 
+// will display wind speed wind.speed
+//will display UV Index and change color to indicate safety Green/yellow/red
+
+
+
+
+
+// display a 5 day forcast in 
+
+weatherFetch()
+tempFetch()
 cityHistory()
+humidityFetch()
